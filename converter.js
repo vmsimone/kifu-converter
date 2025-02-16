8bb9c375b4691debf252a92e62e8a581f407168b
V S <jackuhlantern@gmail.com>
2025-02-15 22:59:16 -0700

    added ambiguity handler

--- converter.js
+++ converter.js
@@ -64,6 +64,7 @@ function organizeMoves(input) {
 
     movesArr.forEach((move, i) => {
         let prevMove = movesArr[i - 1];
+        let player = (i % 2 === 0 ? 'sente' : 'gote')
 
         let moveIsCapture = move.indexOf('x') > -1;
         let moveIsDrop = move.indexOf('*') > -1;
@@ -77,11 +78,14 @@ function organizeMoves(input) {
             organizedArr.push(move);
 
         } else if(moveIsCapture) {
+            // if(moveIsAmbiguous) {
+            //     move = ambiguityHandler(move, player);
+            // }
             move = captureHandler(move, prevMove);
             organizedArr.push(move);
-
-            // add ambiguity handler
-
+        } else if(moveIsAmbiguous) {
+            move = ambiguityHandler(move, player);
+            organizedArr.push(move);
         } else {
             if(moveIsDrop && !moveIsPawnDrop) {
                 move = move + '打';
@@ -92,14 +96,10 @@ function organizeMoves(input) {
 
             organizedArr.push(newMove);
         }
-
-
     });
     console.log(movesArr);
 
     printKifu(organizedArr);
-
-    // console.log(output);
 }
 
 function captureHandler(move, prevMove) {
@@ -107,7 +107,7 @@ function captureHandler(move, prevMove) {
         const piece = move[0];
         const coord = move[2] + move[3]
         let newMove = coord + piece + move.substring(4);
-        return newMove
+        return newMove;
     } else {
         let sameCoord = (prevMove[2] + prevMove[3]) === (move[2] + move[3])
         if(sameCoord) {
@@ -117,8 +117,65 @@ function captureHandler(move, prevMove) {
             const piece = move[0];
             const coord = move[2] + move[3]
             let newMove = coord + piece + move.substring(4);
-            return newMove
+            return newMove;
         }
+    }
+}
+
+function ambiguityHandler(move, player) {
+    const piece = move[0];
+    const originCol = move[1];
+    const originRow = kanjiValue(move[2]);
+    const destinationCol = move[4];
+    const destinationRow = kanjiValue(move[5]);
+
+    let newMove = piece + destinationCol + move[5];
+
+    // it's actually impossible to do this without a board state so.... shrug
+
+    const lateralMove = (originRow === destinationRow || originRow === destinationRow + 1 || originRow === destinationRow - 1);
+    const verticalMove = (originCol === destinationCol);
+
+    if(player == 'sente') {
+        if(lateralMove) {
+            let direction = originCol < destinationCol ? '右' : '左';
+            newMove += direction;
+        } else if (verticalMove) {
+            let direction = originRow > destinationRow ? '直' : '引';
+            newMove += direction;
+        }
+    } else {
+        if(lateralMove) {
+            let direction = originCol > destinationCol ? '右' : '左';
+            newMove += direction;
+        } else if (verticalMove) {
+            let direction = originRow < destinationRow ? '直' : '引';
+            newMove += direction;
+        }
+    }
+    return newMove;
+}
+
+function kanjiValue(kanji) {
+    switch(kanji) {
+        case('一'):
+            return 1
+        case('二'):
+            return 2
+        case('三'):
+            return 3
+        case('四'):
+            return 4
+        case('五'):
+            return 5
+        case('六'):
+            return 6
+        case('七'):
+            return 7
+        case('八'):
+            return 8
+        case('九'):
+            return 9
     }
 }
 
@@ -126,7 +183,7 @@ function printKifu(arr) {
     let kifu = '';
     arr.forEach((move, i) => {
         kifu += `
-        ${i + 1} ${move}`;
+${i + 1} ${move}`;
     })
     console.log(kifu);
     $('#output').val(kifu);
